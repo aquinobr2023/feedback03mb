ec8cca8ad5188dc887c117c8eb3de93060896d13
gasparydeaquinovictor <gasparydeaquinovictor@gmail.com>
2025-03-30 22:47:51 +0000

    fim da criaçnao e estilizaçnao do projeto feedback no mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import starIcon from "./assets/icon-star.svg";
+import illustrationTrankYou from "./assets/illustration-thank-you.svg"
+
 import { useState } from "react";
 
 export function App () {
@@ -44,7 +46,14 @@ export function App () {
     </div>
     ) : (
       <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
-        <p>{notaAvaliacao}</p>
+        <img className="mx-auto mb-6" src={illustrationTrankYou} alt="illustration thankyou" />
+
+        <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You selected {notaAvaliacao} out of 5</p>
+
+        <h1 className="text-2xl font-bold mb-2.5 text-center">Thank you!</h1> 
+
+        <p className=" text-light-grey text-sm text-center">We appreciate you taking the time to give a rating. 
+          If you ever need more support, don’t hesitate to get in touch!</p>
     </div>
     )
   )
